bd2f79af1ba769368ab66b5e89e8529afaeef87d
rhenerose <kenroseryu@gmail.com>
2021-07-19 10:51:48 +0900

    fix convert text to int bug

--- xml_to_txt.py
+++ xml_to_txt.py
@@ -2,12 +2,14 @@ import xml.etree.ElementTree as ET
 import os
 from glob import glob
 
-XML_PATH = './dataset/xml'
-CLASSES_PATH = './class_names/classes.txt'
-TXT_PATH = './dataset/txt/anno.txt'
+XML_PATH = "./dataset/Annotations"                  # dataset xml directory
+CLASSES_PATH = "./class_names/dsit_classes.txt"     # class text path
+TXT_PATH = "./dsit_train.txt"                       # output fila path
 
 
-'''loads the classes'''
+"""loads the classes"""
+
+
 def get_classes(classes_path):
     with open(classes_path) as f:
         class_names = f.readlines()
@@ -16,27 +18,32 @@ def get_classes(classes_path):
 
 
 classes = get_classes(CLASSES_PATH)
-assert len(classes) > 0, 'no class names detected!'
-print(f'num classes: {len(classes)}')
+assert len(classes) > 0, "no class names detected!"
+print(f"num classes: {len(classes)}")
 
 # output file
-list_file = open(TXT_PATH, 'w')
+list_file = open(TXT_PATH, "w")
 
-for path in glob(os.path.join(XML_PATH, '*.xml')):
+for path in glob(os.path.join(XML_PATH, "*.xml")):
     in_file = open(path)
 
     # Parse .xml file
     tree = ET.parse(in_file)
     root = tree.getroot()
     # Write object information to .txt file
-    file_name = root.find('filename').text
+    file_name = root.find("filename").text
     print(file_name)
     list_file.write(file_name)
-    for obj in root.iter('object'):
-        cls = obj.find('name').text 
+    for obj in root.iter("object"):
+        cls = obj.find("name").text
         cls_id = classes.index(cls)
-        xmlbox = obj.find('bndbox')
-        b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
-        list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
-    list_file.write('\n')
+        xmlbox = obj.find("bndbox")
+        b = (
+            int(float(xmlbox.find("xmin").text)),
+            int(float(xmlbox.find("ymin").text)),
+            int(float(xmlbox.find("xmax").text)),
+            int(float(xmlbox.find("ymax").text)),
+        )
+        list_file.write(" " + ",".join([str(a) for a in b]) + "," + str(cls_id))
+    list_file.write("\n")
 list_file.close()
